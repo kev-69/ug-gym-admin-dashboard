a99b4dce1df26932a1678e165538dfeaff6b0e1b
kev-69 <bismarkobuobi19@gmail.com>
2024-12-19 16:33:41 +0000

    better style to button

--- University of Ghana Admin Dashboard/src/pages/Dashboard.jsx
+++ University of Ghana Admin Dashboard/src/pages/Dashboard.jsx
@@ -122,7 +122,7 @@ const Dashboard = () => {
                   </td>
                   <td className="px-6 py-4 text-sm text-gray-700">
                     <button 
-                      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                      className="mt-3 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors"
                       onClick={() => openModal(user)} >
                       View Details
                     </button>
